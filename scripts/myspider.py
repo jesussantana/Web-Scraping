a8714d8dbb1823e757e0eca7dfa16d6b8c4415ee
jesussantana <jesus@esmarketingdigital.com>
2021-06-30 15:26:26 +0200

    Update Web Scraping

---
+++ scripts/myspider.py
@@ -0,0 +1,22 @@
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+    name = "spider"
+
+    def start_requests(self):
+        urls = [
+            'http://esmarketingdigital.es/',
+            'http://esmarketingdigital.es/',
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        page = response.url.split("/")[-2]
+        filename = f'esmarketingdigital_copy.html'
+        #filename = f'spyder-{page}.html'
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log(f'Saved file {filename}')
+ 
